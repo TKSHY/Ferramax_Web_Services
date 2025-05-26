4bf30d60991817fd22337e781f28864ded73d039
fleonnn <133935043+fleonnn@users.noreply.github.com>
2025-05-26 15:39:23 -0400

    correcion de catalogo

--- webservice/urls.py
+++ webservice/urls.py
@@ -6,6 +6,5 @@ from webservice.api.register_api import register_user
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('register/', views.register, name='register'),
-    path('catalog/', views.catalog, name='catalog'),
     path("api/register/", register_user, name="register_user"),
 ]
